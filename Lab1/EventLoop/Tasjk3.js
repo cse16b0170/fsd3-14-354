7d25134c1c4109d7430b81ba790b1c4d5b62bc32
cse16b0170 <a9475210@gmail.com>
2026-07-23 16:26:08 +0530

    change

--- Lab1/EventLoop/Tasjk3.js
+++ Lab1/EventLoop/Tasjk3.js
@@ -2,4 +2,4 @@ console.log("1: start");
 setTimeout(() => console.log("2: setTimeout"),0);
 setImmediate(() => console.log("3: setImmediate"));
 process.nextTrick(() => console.log("4: nextTrick"));
-console.log("4: end");
+console.log("5: end");
