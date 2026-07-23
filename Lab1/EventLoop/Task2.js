2c56b5bc48d4449513447699841511e9bd6a9836
cse16b0170 <a9475210@gmail.com>
2026-07-23 15:39:01 +0530

    task2 commit

---
+++ Lab1/EventLoop/Task2.js
@@ -0,0 +1,15 @@
+const f1=()=> {
+    console.log("f1");
+};
+
+const f2=()=> {
+    console.log("f2");
+};
+
+const main=()=> {
+    console.log("main");
+    setTimeout(f1, 5000);
+    f2();
+    console.log("end");
+};
+main();
