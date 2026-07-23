7db3c39c8411247240e4d0916d8f0af2fbc5f508
cse16b0170 <a9475210@gmail.com>
2026-07-23 16:09:40 +0530

    changes in task2 commit

--- Lab1/EventLoop/Task2.js
+++ Lab1/EventLoop/Task2.js
@@ -1,3 +1,14 @@
+import fs from "fs/promises";
+
+const writeData = async()=>{
+    try{
+        console.log("about to write...");
+        await fs.writeFile("stud.txt","Name:Raman singh");
+        console.log("Data written successfully");
+    }catch(error){
+        console.log(error);
+    }
+}
 const f1 = () => {
     console.log("f1");
 };
@@ -6,10 +17,17 @@ const f2 = () => {
     console.log("f2");
 };
 
+const f3 = () => {
+    console.log("f3");
+};
+
 const main = () => {
     console.log("main");
-    setTimeout(f1, 0);
-    f2();
+    setTimeout(f1, 5000);
+    setInterval(f2, 1000);
+    setImmediate(f2);
+     process.nextTick(f3); //promise
+     writeData();
     console.log("end");
 };
 main();
