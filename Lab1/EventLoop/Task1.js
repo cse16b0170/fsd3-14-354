159f619cbccf6e90be9412c388521aabe6898670
cse16b0170 <a9475210@gmail.com>
2026-07-23 15:20:11 +0530

    change in task 1

--- Lab1/EventLoop/Task1.js
+++ Lab1/EventLoop/Task1.js
@@ -1,19 +1,28 @@
 const f1 =()=> {
-    console.log("f1");
+    console.log("f1 start");
+    f2();
+    console.log("f1 running");
+    console.log("f1 end");
 };
 
 const f2 =()=> {
-    console.log("f2");
+    console.log("f2 start");
+    f3();
+    console.log("f2 running");
+    console.log("f2 end");
 };
 
 const f3 =()=> {
-    console.log("f3");
+    console.log("f3 start");
+    console.log("f3 running");
+    console.log("f3 end");
 };
 
-function main() {
+
+function main(){
+    console.log("main starts");
     f1();
-    f2();
-    f3();
-    console.log("end main");
+    console.log("main running");
+    console.log("main ends");
 };
 main();
