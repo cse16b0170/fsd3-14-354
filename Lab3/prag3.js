bf5570093f156bb140ab373af63082661d6e0d42
cse16b0170 <a9475210@gmail.com>
2026-09-01 09:22:23 +0530

    commit in lab 3

--- Lab3/prag3.js
+++ Lab3/prag3.js
@@ -1,20 +1,24 @@
-import http from 'http';
+import http from "http";
 
-const server = http.createServer((req,res) => {
-  if (http.url == "/") {  
-    res.end("<h2>Welcome to Serverside</h2>")
-    
+const server = http.createServer((req, res) => {
+  if (req.url == "/") {
+    res.end("<h1>Home Page</h1>");
+  } else if (req.url == "/about") {
+    res.end("<h1>About Us Page</h1>");
+  } else if (req.url == "/product")
+    res.end(`<h1>Mobile Phone</h1>
+                <h2>Price: 25000</h2>
+                <p>Discount: 5%</p>
+                <a href='#'>Buy Now</a>
+            `);
+  else {
+    res.statusCode = 404;
+    res.end(`
+            <h1>404, Not found</h1>
+            <p>Page not found</p>
+            <a href='/'>Home</a>
+            `);
+  }
 });
 
-server.listen(5000, () => {
-    console.log("Server is running")
-   
-});
-else {
-    res statusCode =404;
-    re.end(
-           <h1>404, Not found<h1/>
-           <p>Page not found<p/>
-           
-    )
-}
+server.listen(4444, () => console.log("Server is running..."));
