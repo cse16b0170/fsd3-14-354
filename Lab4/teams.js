fee3d5b63f99d43ac6f704dbe48c18148f2cc2f3
cse16b0170 <a9475210@gmail.com>
2026-09-17 13:16:43 +0530

    comkit lab 4

--- Lab4/teams.js
+++ Lab4/teams.js
@@ -1,44 +1,42 @@
-let teams = [{
-    
-    id:1,
+let teams = [
+    {id:1,
     tname:"Rusty",
-    tl : 'Aashish Raj Singh',
-    email :'ashi.raj@gmail.com',
-    members : 6,
+    tl:'Aashish Raj Singh',
+    email: "Ashi.raj@gamil.com",
+    members: 6,
 },
-
-{
-    
-    id:2,
-    tname:"Assemblers",
-    tl : 'Ayush Belwal',
-    email :'ayu.bel@gmail.com',
-    members : 6,
+    {id:2,
+    tname:"code Crafter",
+    tl:'Manisha Singh',
+    email: "mani.singh@gamil.com",
+    members: 5,
 },
 ];
 
 let nextId = 3;
 
-export const getAllTeams = () => teams;
+export const getAllTeams = ()=> teams;
 
-export const getTeamById =  ( id ) => teams.find((team)=> team.id === id);
+export const getTeamById = (id)=> teams.find((team)=> team.id === id);
 
-export const addTeam = (newTeam =>{
-    const team = {id: nextId++, newTeam};
+export const addTeam = (newTeam) => {
+    const team = {id:nextId++, newTeam};
     teams.push(team);
     return team;
-});
+}
 
-export const updateTeamById = (id,updateTeam) =>{
+export const updateTeamById = (id, updateTeam)=>{
     const team = getTeamById(id);
-    if(!team) return null;
-    Object.assign(team,updateTeam);
+    if(!team){
+        return null;
+    }
+    Object.assign(team, updateTeam);
     return team;
-};
+}
 
-export const deleteTeam = (id) => {
+export const deleteTeam = (id)=>{
     const index = teams.findIndex((team)=>team.id === id);
-    if(index === -1) return false;
-    teams.splice(index,1);
+    if(index == -1) return false;
+    teams.splice(index, 1);
     return true;
-};
+}
